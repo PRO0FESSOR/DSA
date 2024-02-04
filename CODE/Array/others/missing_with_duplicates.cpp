82d77134c8f938622e3dbe91110ccaae91bdc99b
PRO0FESSOR <siddharth96549@gmail.com>
2024-02-04 16:15:13 +0530

    76. Minimum Window Substring

---
+++ CODE/Array/others/missing_with_duplicates.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n = 5;
+    int arr[n] = {1,5,3,3,4};
+
+    for(int i=0;i<n;i++)
+    {
+        int index = abs(arr[i]);
+
+        if(arr[index-1] > 0)
+        {
+            arr[index-1] *= -1;
+        }
+    }
+
+    int ans;
+
+    for(int i=0;i<n;i++)
+    {
+        if(arr[i]>0)
+        {
+            ans = i+1;
+        }
+    }
+    cout<<ans<<endl;
+
+    return 0;
+}
