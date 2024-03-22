37839ea48bddbf5470aec52692cc8031402d29a4
PRO0FESSOR <siddharth96549@gmail.com>
2024-03-22 22:47:28 +0530

    234. Palindrome Linked List

---
+++ CODE/lc/easy/234.cpp
@@ -0,0 +1,21 @@
+
+
+// 234. Palindrome Linked List
+
+// class Solution {
+// public:
+//     ListNode* front;
+//     bool check(ListNode* Node){
+//         if (Node!=NULL) {
+//             if (!check(Node->next)) return 0;
+//         //    cout<<"("<<Node->val<<","<<front->val<<")";
+//             if (Node->val!=front->val) return 0;
+//             front=front->next;
+//         }
+//         return 1;
+//     }
+//     bool isPalindrome(ListNode* head) {
+//         front=head;
+//         return check(head);
+//     }
+// };
