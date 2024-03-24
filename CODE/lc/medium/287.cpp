cf5ddf9691ac7b47306e641afb4089413c932863
PRO0FESSOR <siddahrth96549@gmail.com>
2024-03-24 22:44:24 +0530

    duplicate it

--- CODE/lc/medium/287.cpp
+++ CODE/lc/medium/287.cpp
@@ -1,24 +1,28 @@
-// class Solution {
-// public:
-//     int findDuplicate(vector<int>& nums) {
+class Solution {
+public:
+    int findDuplicate(std::vector<int>& nums) {
+        int left = 1;
+        int right = nums.size() - 1;
 
-//         int n = nums.size();
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            int count = 0;
 
-//         for(int i=0;i<n;i++)
-//         {
-//             int index  =  abs(nums[i]);
+            // Count the numbers less than or equal to mid
+            for (int num : nums) {
+                if (num <= mid) {
+                    count++;
+                }
+            }
 
-//             if(nums[index]<0)
-//             {
-//                 return index;
-//             }
-//             else{
-//                 nums[index] *= -1;
-//             }
-//         }
+            // If count is greater than mid, the duplicate lies in the left half
+            if (count > mid) {
+                right = mid;
+            } else { // Otherwise, it lies in the right half
+                left = mid + 1;
+            }
+        }
 
-//         return -1;
-        
-//     }
-// };
-
+        return left;
+    }
+};
