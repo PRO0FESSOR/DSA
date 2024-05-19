3f9a59d9de8afcdf69aa72f66a3788a6f9a82418
PRO0FESSOR <siddharth96549@gmail.com>
2024-05-19 14:28:48 +0530

    lc 20

---
+++ CODE/Stack/valid_parantheses.cpp
@@ -0,0 +1,54 @@
+//leetcode easy 20
+
+
+// class Solution {
+// public:
+//     bool isValid(string s) {
+        
+//         stack<char>st;
+        
+//         for(int i=0;i<s.length();i++){
+//             char ch = s[i];
+            
+//             if(ch=='(' || ch=='{' || ch=='[')
+//             {
+//                 st.push(ch);
+//             }
+//             else{
+//                 //cancellation
+//                 if(!st.empty())
+//                 {
+//                     char topchar = st.top();
+                    
+//                     if(topchar=='(' && ch == ')')
+//                     {
+//                         st.pop();
+//                     }
+//                     else if(topchar=='{' && ch == '}')
+//                     {
+//                         st.pop();
+//                     }
+//                     else if(topchar=='[' && ch == ']')
+//                     {
+//                         st.pop();
+//                     }
+//                     else{
+//                         return false;
+//                     }
+//                 }
+//                 else{
+//                     return false;
+//                 }
+//             }
+            
+//             }
+//         if(st.empty())
+//             {
+//                 return true;
+//             }
+//             else{
+//                 return false;
+//             }
+        
+//     }
+// };
