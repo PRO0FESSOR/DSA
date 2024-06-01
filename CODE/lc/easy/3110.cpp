ecdc845f4c40bdea3699db943567a8f4b472fe6d
PRO0FESSOR <siddharth96549@gmail.com>
2024-06-02 00:41:05 +0530

    3110. Score of a String

---
+++ CODE/lc/easy/3110.cpp
@@ -0,0 +1,16 @@
+
+// 3110. Score of a String
+
+// class Solution {
+// public:
+//     int scoreOfString(string s) {
+//         int score = 0;
+//         // Iterate over all indices from 0 to the second-to-last index
+//         // Calculate and accumulate the absolute difference of ASCII values
+//         // between adjacent characters
+//         for (int i = 0; i < s.size() - 1; ++i) {
+//             score += abs(s[i] - s[i + 1]);
+//         }
+//         return score;
+//     }
+// };
