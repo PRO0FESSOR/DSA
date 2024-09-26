c2c8ea653b46b0c097bb3a0230cdd612087ef465
PRO0FESSOR <siddharth96549@gmail.com>
2024-09-26 12:27:13 +0530

    all duplicates o of n solution

--- array/all_duplicates.cpp
+++ array/all_duplicates.cpp
@@ -21,40 +21,88 @@
 
 
 //solution 2 using array of 0's 
+/**/
+/*#include<iostream>*/
+/*#include<vector>*/
+/**/
+/*auto all_duplicates(std::vector<int>&nums,std::vector<int>&ans) -> void {*/
+/**/
+/*  auto values = std::vector<int>(nums.size(),0);*/
+/**/
+/*  for (auto i : nums) {*/
+/*        if (values[i - 1] == 1) {*/
+/*            ans.push_back(i);*/
+/*        } else {*/
+/*            values[i - 1] = 1;*/
+/*        }*/
+/*}*/
+/*}*/
+/**/
+/*int main(){*/
+/**/
+/*  //input output optimization*/
+/*  std::ios::sync_with_stdio(false);*/
+/*  std::cin.tie(0);*/
+/*  std::cout.tie(0);*/
+/**/
+/*  auto nums = std::vector<int>{1,1,2,2,3,4};*/
+/*  std::vector<int>ans;*/
+/**/
+/*  all_duplicates(nums,ans);*/
+/**/
+/*  for(auto &i : ans){*/
+/*    std::cout<<i<<" ";*/
+/*  }*/
+/*  std::cout<<std::endl;*/
+/**/
+/*  return 0;*/
+/**/
+/*}*/
+
+
+// solution 3 o(n) using in place updation for marking  
+
 
 #include<iostream>
 #include<vector>
 
-auto all_duplicates(std::vector<int>&nums,std::vector<int>&ans) -> void {
+auto all_duplicates(std::vector<int>&nums) -> void {
   
-  auto values = std::vector<int>(nums.size(),0);
+  auto n = nums.size();
+  std::cout<<"size is "<<n<<" "<<std::endl;
 
-  for (auto i : nums) {
-        if (values[i - 1] == 1) {
-            ans.push_back(i);
-        } else {
-            values[i - 1] = 1;
-        }
+  for(auto val : nums){
+    nums[val-1] += n;
+  }
+
+  std::vector<int>ans;
+
+  for(size_t i=0;i<n;++i){
+    if((nums[i]/n)>1) ans.push_back(i+1);
+  }
+
+  for(auto val : ans){
+    std::cout<<val<<" ";
+  }
+  std::cout<<std::endl;
+
 }
-}
+
 
 int main(){
-
-  //input output optimization
+  
   std::ios::sync_with_stdio(false);
   std::cin.tie(0);
   std::cout.tie(0);
 
   auto nums = std::vector<int>{1,1,2,2,3,4};
-  std::vector<int>ans;
-  
-  all_duplicates(nums,ans);
 
-  for(auto &i : ans){
-    std::cout<<i<<" ";
-  }
-  std::cout<<std::endl;
+  all_duplicates(nums);
 
   return 0;
 
+
 }
+
+
+
