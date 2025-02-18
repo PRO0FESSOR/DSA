f28ec2fad60e5a6e77959c810b0c697156c57b10
PRO0FESSOR <siddharth96549@gmail.com>
2025-02-18 13:34:56 +0530

    totalOccurance

---
+++ binary_search/totalOccurance.cpp
@@ -0,0 +1,54 @@
+
+// find the leftmost and rightmost occurance of a target in an array 
+
+#include<iostream>
+#include<vector>
+
+auto search(std::vector<int>nums,int target) -> void {
+  
+  auto start = 0;
+  auto end = nums.size()-1;
+
+  auto leftMostIndex = -1;
+  auto rightMostIndex = -1;
+
+  while(start<=end){
+
+    auto mid  = start + (end-start)/2;
+
+    if(nums[mid]==target){
+      leftMostIndex = mid;
+      end = mid-1;
+    }
+    else if(target > nums[mid]) start = mid+1;
+    else end = mid-1;
+  }
+
+  start = 0;
+  end = nums.size()-1;
+
+  while(start<=end){
+    auto mid  = start + (end-start)/2;
+
+    if(nums[mid]==target){
+      rightMostIndex = mid;
+      start = mid+1;
+    }
+    else if(target > nums[mid]) start = mid+1;
+    else end = mid-1;
+   }
+
+  std::cout<<"The total number of occurances are -> "<<rightMostIndex-leftMostIndex+1<<std::endl;
+
+  return;
+
+
+}
+
+
+int main(){
+  auto target = 3;
+  auto nums = std::vector<int>{1,2,3,3,3,3,3,3,5,6};
+  search(nums,target);
+  return 0;
+}
